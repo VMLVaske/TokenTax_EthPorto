c809d4b3b1cebf391f83f3b697093d84796eac71
Lilith <valerie.vaske@gmail.com>
2023-03-18 07:10:02 +0000

    done

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,18 +1,18 @@
-require("dotenv").config()
+require("dotenv").config({ path: '../.env' })
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-  defaultNetwork: process.env.NETWORK,
+    solidity: "0.8.18",
+    defaultNetwork: process.env.NETWORK,
     networks: {
-        goerli: {
+        "goerli": {
             url: process.env.GOERLI_NODE_URL || "",
             accounts: [process.env.WALLET_PRIVATE_KEY]
         },
-        mainnet: {
-            url: process.env.MAINNET_NODE_URL || "",
-            accounts: [process.env.WALLET_PRIVATE_KEY],
-        },
+        "mantle-testnet": {
+            url: "https://rpc.testnet.mantle.xyz/",
+            accounts: [process.env.WALLET_PRIVATE_KEY]
+        }
     },
 };
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -7,21 +7,24 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+/*   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
-
   const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
   const Lock = await hre.ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
   await lock.deployed();
-
   console.log(
     `Lock with ${ethers.utils.formatEther(
       lockedAmount
     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  ); */
+
+  const fundingTrackerFactory = await hre.ethers.getContractFactory("FundingTracker");
+  const fundingTracker = await fundingTrackerFactory.deploy();
+
+  await fundingTracker.deployed();
+
+  console.log("FundingTracker deployed to:", fundingTracker.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
